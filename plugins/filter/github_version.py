1d734703220c25f4ba46be62b9e19d29086a3843
mikavl <117277985+mikavl@users.noreply.github.com>
2023-08-18 22:52:14 +0300

    Add untested filter plugin

---
+++ plugins/filter/github_version.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import urllib.request
+import re
+
+class FilterModule(object):
+
+    def filters(self):
+        return {
+            "github_latest": self.github_latest,
+        }
+
+    def github_latest(self, repository):
+        url = "https://github.com/{0}/releases/latest".format(repository)
+        regex = 'href="/{0}/releases/tag/(v[0-9]\.[0-9]\.[0-9])"'.format(repository)
+
+        with urllib.request.urlopen(url) as response:
+            content = response.read().decode(response.headers.get_content_charset())
+            result = re.search(regex, content)
+            return result.group(1)
